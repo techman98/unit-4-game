61a9b76ac20a78acc0c85499b33d1730efd64b3e
Techman <wizmar.tyler@gmail.com>
2018-12-16 18:19:23 -0600

    Game is done and working. Never figured out how to call a function inside another function without copying the entire thing.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # unit-4-game
+This is a game that generates a random number at the start of the game, and when a user clicks on a gem their score goes up by the randomly generated number assigned to that gem. The goal is to get to the target score without going over.
+
+This project was built to help teach me the syntax of jQuery and how to use it properly in functions and methods.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,32 +5,51 @@ $(document).ready(function () {
     var ranNum;
     var wins = 0;
     var losses = 0;
-    console.log("Get to " + compChoice);
+    $("#Goal").text(compChoice);
 
+    //Sets a random number value to each instance of .crystal
     $(".crystal").each(function (index) {
         ranNum = Math.floor(Math.random() * 12) + 1;
         $(this).data("value", ranNum);
-        console.log("Gem # " + index + "value is " + ranNum)
 
     })
-
+    //When user clicks the game checks certain conditions before executing.
     $(".crystal").on("click", function () {
 
 
         if (storedNum > compChoice) {
-            alert("You fucking suck.");
+            $("#endGameStatement").text("Now you've gone and done it. The big, green, gem troll is after you now. You greedy bastard.");
             losses++
             $("#losses").text(losses);
+            storedNum = 0;
+            compChoice = Math.floor(Math.random() * 120) + 19;
+            //I copied the function here, because I couldn't find out how to call a function that doesn't have a name.
+            $(".crystal").each(function (index) {
+                ranNum = Math.floor(Math.random() * 12) + 1;
+                $(this).data("value", ranNum);
 
+            })
+            $("#Score").text(storedNum);
+            $("#Goal").text(compChoice);
 
         } else if (storedNum === compChoice) {
-            $("#winStatement").text("You fucking don't suck!");
+            $("#endGameStatement").text("Look at you! Ballin' like a G.");
             wins++
             $("#wins").text(wins);
+            storedNum = 0;
+            compChoice = Math.floor(Math.random() * 120) + 19;
+            //Read above comment
+            $(".crystal").each(function (index) {
+                ranNum = Math.floor(Math.random() * 12) + 1;
+                $(this).data("value", ranNum);
+
+            })
+            $("#Score").text(storedNum);
+            $("#Goal").text(compChoice);
 
         } else {
             storedNum = storedNum + ($(this).data().value);
-            console.log("Your score is " + storedNum);
+            $("#Score").text(storedNum);
 
         }
     })
